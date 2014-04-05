da6f3e178b950db8a7d780b2b4c0feb41dd1b7f8
frenkie <frank@frankbosma.nl>
2014-04-05 10:17:58 +0200

    querySelectorAll over jQuery and better docs

--- README.md
+++ README.md
@@ -1,9 +1,26 @@
 # give me kittens
 
-Show kittens on any website. Copy paste the `kittens.js` script in your developer tools and call the `giveMeKittens` function with your DOM selectors
+Show kittens on any website. Copy paste the `kittens.js` script in your developer tools,
+call the `giveMeKittens` function with your DOM selector string and
+tada, instant kittens!
+
+## api
+
+### giveMeKittens
+    /**
+     * Turns all your selected items into kittens
+     * @param {String} selector a document.querySelectorAll valid selector string
+     * @param {Object} [dimensionsThreshold] Optional minimal dimensions before
+     *                  elements are kittenized, defaults to width and height of 50px
+     *      @param {Number} [dimensionsThreshold.width]
+     *      @param {Number} [dimensionsThreshold.width]
+     */
+    function giveMeKittens( selector, dimensionsThreshold )
+
+Check out the [document.querySelectorAll](https://developer.mozilla.org/en-US/docs/Web/API/Document.querySelectorAll) docs for more info
 
 ## examples
 
-### Nu.nl
-
-`giveMeKittens( 'img[xtclib], .image img, .trackevent img, iframe[id^=scr_]' )`
+### http://nu.nl
+A Dutch news website
+`giveMeKittens( 'img[xtclib], .image img, .trackevent img, iframe[id^=scr_]' );`
--- kittens.js
+++ kittens.js
@@ -1,17 +1,58 @@
-var giveMeKittens = function ( selector ) {
-    jQuery( selector ).each(function () {
-		var image;
-        var dimensions = {};
-        dimensions.width = $(this).width();
-        dimensions.height = $(this).height();
+/**
+ * Turns all your selected items into kittens
+ * @param {String} selector a document.querySelectorAll valid selector string
+ * @param {Object} [dimensionsThreshold] Optional minimal dimensions before
+ *                  elements are kittenized, defaults to width and height of 50px
+ *      @param {Number} [dimensionsThreshold.width]
+ *      @param {Number} [dimensionsThreshold.width]
+ */
+var giveMeKittens = function ( selector, dimensionsThreshold ) {
 
-        if ( dimensions.width > 50 &&
-             dimensions.height > 50
+    dimensionsThreshold = dimensionsThreshold || {
+        width: 50,
+        height: 50
+    };
+
+    Array.prototype.forEach.call( document.querySelectorAll( selector ), function ( element ) {
+
+        var imageUrl, tagName;
+
+        var dimensions = {
+            width: element.clientWidth,
+            height: element.clientHeight
+        };
+
+        if ( dimensions.width > dimensionsThreshold.width &&
+             dimensions.height > dimensionsThreshold.height
            ) {
 
-           	this.src = 'http://placekitten.com/'+
-                        dimensions.width +'/'+
-                        dimensions.height;
+            imageUrl = 'http://placekitten.com/'+
+                                    dimensions.width +'/'+
+                                    dimensions.height;
+
+            tagName = element.tagName.toLowerCase();
+
+            switch ( tagName ) {
+
+                case 'img':
+                case 'object':
+                case 'embed':
+                case 'iframe':
+                    element.src = imageUrl;
+                    break;
+
+                case 'hr':
+                    // oh noes kittens
+                    throw Error( 'oh noes kittens for your '+ tagName );
+                    break;
+
+                default:
+                    element.style.width = dimensions.width +'px';
+                    element.style.height = dimensions.height +'px';
+                    element.style.backgroundImage = 'url("'+ imageUrl +'")';
+                    element.innerHTML = '';
+                    break;
+            }
         }
     });
 };
